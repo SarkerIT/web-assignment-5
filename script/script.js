a905a5b9b040ac9be06985c11a2691cecef75794
SarkerIT <sarker.data@gmail.com>
2024-02-22 23:15:12 -0600

    seat selection upto four, bg-colot and disabled fixed

--- script/script.js
+++ script/script.js
@@ -1,35 +1,48 @@
 // alert("loaded");
 
-const allSeat = document.getElementsByClassName("select-seat");
+const allSeat = document.getElementsByClassName("select-seat-btn");
 
 //seat selection and showing in the cart table
 for (const seat of allSeat) {
   seat.addEventListener("click", function (event) {
-    // let number = 4;
-
     const totalSelectedSeatDiv = document.getElementById(
       "total-ticket-selected"
     );
 
+    // update cart and total seat upto four seats
+    const chosenSeatNum = getConvertedValue("total-seat-selected");
+    const totalSeatNum = getConvertedValue("total-seat-capacity");
+
+    if (chosenSeatNum + 1 > 4) {
+      alert("Seat limit crossed");
+      return;
+    } else {
+      document.getElementById("total-seat-selected").innerText =
+        chosenSeatNum + 1;
+      document.getElementById("total-seat-capacity").innerText =
+        totalSeatNum - 1;
+      appendChosenSeatDiv();
+    }
+
     // set chosen ticket info on the fly
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("flex");
-    newDiv.classList.add("justify-between");
+    function appendChosenSeatDiv() {
+      const newDiv = document.createElement("div");
+      newDiv.classList.add("flex");
+      newDiv.classList.add("justify-between");
 
-    const p1 = document.createElement("p");
-    p1.innerText = event.target.innerText;
+      const p1 = document.createElement("p");
+      p1.innerText = event.target.innerText;
+      const p2 = document.createElement("p");
+      p2.innerText = "Economy";
+      const p3 = document.createElement("p");
+      p3.innerText = "550";
 
-    const p2 = document.createElement("p");
-    p2.innerText = "Economy";
+      newDiv.appendChild(p1);
+      newDiv.appendChild(p2);
+      newDiv.appendChild(p3);
 
-    const p3 = document.createElement("p");
-    p3.innerText = "550";
-
-    newDiv.appendChild(p1);
-    newDiv.appendChild(p2);
-    newDiv.appendChild(p3);
-
-    totalSelectedSeatDiv.appendChild(newDiv);
+      totalSelectedSeatDiv.appendChild(newDiv);
+    }
 
     // --- call total cost --
     updateTotalCost("total-cost");
@@ -37,24 +50,21 @@ for (const seat of allSeat) {
     // -- call grand total --
     updateGrandTotal();
 
-    // call update seat num in the cart
-    updateSeatNumbers();
-
     //  --- update bg-color upon selection
-    const px = event.target.classList;
-    console.log(px);
-    px.add("bg-green-300");
+    // const px = event.target.classList;
+    // px.add("bg-green-300");
+    // disable button
+    console.log(event.target.parentNode);
+    event.target.setAttribute("disabled", false);
+    event.target.style.backgroundColor = "limegreen";
   });
 }
 
 // update total price
 function updateTotalCost(value) {
   const totalInitialPrice = getConvertedValue(value);
-
   const totalFinalPrice = totalInitialPrice + 550;
-
   document.getElementById("total-cost").innerText = totalFinalPrice;
-
   return totalFinalPrice;
 }
 
@@ -75,19 +85,6 @@ function updateGrandTotal(codeApply) {
   }
 
   document.getElementById("grand-total-cost").innerText = totalPrice;
-}
-
-// update cart seat num in cart
-function updateSeatNumbers() {
-  let chosenSeatNum = getConvertedValue("total-seat-selected");
-  chosenSeatNum = chosenSeatNum + 1;
-  // console.log(chosenSeatNum);
-  document.getElementById("total-seat-selected").innerText = chosenSeatNum;
-
-  let totalSeatNum = getConvertedValue("total-seat-capacity");
-
-  totalSeatNum = 8 - chosenSeatNum;
-  document.getElementById("total-seat-capacity").innerText = totalSeatNum;
 }
 
 // === get a value from a class
